c4b99ef2275349df48a2cedc7ce969998f929d30
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2025-05-05 19:06:23 +0800

    Trim the item name

--- procedure.js
+++ procedure.js
@@ -3,7 +3,7 @@ import terminalLink from "terminal-link";
 export async function handleItem(item) {
   const { c2cItemsId, c2cItemsName, price } = item;
   const link = terminalLink(
-    c2cItemsName,
+    `${c2cItemsName}`.trim(),
     `https://mall.bilibili.com/neul-next/index.html?page=magic-market_detail&noTitleBar=1&itemsId=${c2cItemsId}&from=market_index`
   );
   console.log(`¥${(price / 100).toString().padEnd(10, " ")}`, link);
